d86605ca9708a5ff40f094c11d5522f2031b0505
Nunna Rama Chandu <ramachandununna@gmail.com>
2025-02-17 11:47:10 +0530

    server.js

--- server.js
+++ server.js
@@ -1,17 +1,47 @@
-const express = require("express");
+const { error } = require('console');
+const express = require('express');
+const app = express() ;
+const port = 8000 ;
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+app.use(express.json());
 
-app.use(express.json()); // Middleware to parse JSON requests
-
-// GET endpoint
-app.get("/", (req, res) => {
-    res.send("Hello, Render Deployment!");
+app.get('/', (req,res) => {
+    res.send("Hello, express server is running");
 });
 
+app.listen(port, ()=>{
+    console.log(`Server is running at http://localhost:${port}`);
+});
 
-// Start server
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.post('/signup', (req,res) => {
+    const { username, email, password, dateOfBirth } = req.body;
+
+    if (!username){
+        return res.status(400).json({ error: "Username cannot be empty"}) ; 
+    }
+
+    if (!email){
+        return res.status(400).json({ error: "Email cannot be empty"}) ;
+    }
+
+    if (!password){
+        return res.status(400).json({ error: "Password cannot be empty"}) ;
+    }
+
+    if (password.length > 8 && password.length <= 16){
+        return res.status(400).json({ error: "Password length should be greater than 8 or less than or equal to 16 "});
+    }
+
+    if (!dateOfBirth){
+        return res.status(400).json({ error: "Date Of Birth cannot be empty"});
+    }
+
+    res.status(201).json({
+        message: "User signed up successfully",
+        user: {
+            username,
+            email,
+            dateOfBirth,
+        }
+    });
 });
